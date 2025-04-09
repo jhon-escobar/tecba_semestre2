ed8d2ef642b9772f544461e8729cfb174212735e
jhon escobar <jhonmiltonescobarsaavedra@gamil.com>
2025-04-08 21:07:29 -0400

    push

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # tecba_semestre2
+
 Programacion y Diseños Web 1
 
 nombre y apellido: Jhon Escobar
 
 lab 1
 
-sabias que jesucristo en sumo sacerdote como nuestro padre celestial, destino 
-
+sabias que jesucristo es sumo sacerdote como nuestro padre celestial, Our destiny
--- python/lista.py
+++ python/lista.py
@@ -1,9 +1,28 @@
-lista_notas =[]
-print ("insgrese las  notas de lsoestudiante ")
 
-nombre = input('ingrese el nombre del estudiante ')
-lista_notas.append(nombre)
-notas = int(input('ingrese su nota '))
-lista_notas.append(notas)
+# lista_notas =[]
+# print ("insgrese las  notas de lsoestudiante ")
+
+# nombre = input('ingrese el nombre del estudiante ')
+# lista_notas.append(nombre)
+# notas = int(input('ingrese su nota '))
+# lista_notas.append(notas)
                   
-print(lista_notas)
+# print(lista_notas)
+
+
+# lista = ['maracuya','coco', 'pitajay']
+# print(lista)
+# lista.append( 'frutaepan')
+# print(lista)
+# lista.remove('coco')
+# print(lista)
+
+def maximo_numero(lista):
+    if not lista:
+         return None
+    return max(lista)
+
+numeros = [3, 9, 10, 45,888]
+resultado = maximo_numero(numeros)
+print(f"el valor maximo de la lista es ;{resultado}")
+
